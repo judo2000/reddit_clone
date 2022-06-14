40b0c62bfaf568e3b25fc50fbbb842f62adbf851
Stephen Moore <judo2000@comcast.net>
2022-06-14 15:41:02 -0600

    Setup express server with apollo-server-express and type-graphql

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
-
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: 'My first post - AGAIN!' });
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post);
-  // console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000');
+  });
 };
 
 main().catch((err) => {
